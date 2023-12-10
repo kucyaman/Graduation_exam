3e475572ade1fc78456c67cb101776958bc7f8c5
くちゃまん <134458801+kucyaman@users.noreply.github.com>
2023-12-11 08:16:41 +0900

    Update 20231210024600_create_pages.rb

--- backend/db/migrate/20231210024600_create_pages.rb
+++ backend/db/migrate/20231210024600_create_pages.rb
@@ -2,8 +2,8 @@ class CreatePages < ActiveRecord::Migration[7.0]
   def change
     create_table :pages do |t|
       t.integer :page_number, null: false
-      t.integer :content_type
-      t.integer :content
+      t.string :content_type
+      t.string :content
       t.references :photo, null: false, foreign_key: true
       t.references :book, null: false, foreign_key: true
 
