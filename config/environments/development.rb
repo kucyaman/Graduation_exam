2c5c2ac9999e3b93af023cbc02c3f6090108a81e
くちゃまん <134458801+kucyaman@users.noreply.github.com>
2023-12-21 13:06:43 +0900

    :rocket:update デプロイに関する修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,8 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  config.hosts << "bookiss.onrender.com"
+
   # Enable server timing
   config.server_timing = true
 
