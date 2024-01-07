6b9fc1c31d1462697235ee46567630fa1868f30e
kucyaman <joinus.ibuki@gmail.com>
2024-01-07 18:13:52 +0900

    :sparkles:feat pageテーブルが３つ同時に作成できる機能を作成しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "home#top"
 
-  resources :books, only: %i[index new create]
+  resources :books, only: %i[index new create] do
+    resources :pages, only: %i[new create]
+  end
 
   post '/user_sessions/callback', to: 'user_sessions#callback'
   delete 'logout', to: 'user_sessions#destroy', as: :logout
