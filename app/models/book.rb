b3c2079c5dd4bba14f3cd1e7f6d04878211873af
kucyaman <joinus.ibuki@gmail.com>
2024-01-07 18:05:33 +0900

    :wrench:add bookとpageのネストを追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :pages, dependent: :destroy
 
+  accepts_nested_attributes_for :pages
 
   validates :title, presence: true
   validates :design_type, presence: true
