62f93babb7051d526357af392820a6ab25bebf58
kucyaman <joinus.ibuki@gmail.com>
2024-01-07 18:02:07 +0900

    [change] books_controllerのリダイレクト先を変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,13 +6,12 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     @book = current_user.books.build(book_params)
 
     if @book.save
-      redirect_to books_path
-      flash[:success] = "Bookの作成に成功しました"
+      redirect_to new_book_page_path(book_id: @book.id), success: "Bookの作成に成功しました"
     else
       flash.now[:danger] = "Bookの作成に失敗しました"
       render :new, status: :unprocessable_entity
